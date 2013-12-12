6e2ecd0e80f4fb56605e6e1501696601dede8ea3
Kai Diefenbach <kai.diefenbach@iqpp.de>
2013-12-12 16:39:19 +0000

    Add ability to pass context to ifhasperm tag.

--- permissions/templatetags/permissions_tags.py
+++ permissions/templatetags/permissions_tags.py
@@ -1,19 +1,21 @@
 # django imports
 from django import template
+from django.template import Variable
 
 import permissions.utils
 register = template.Library()
 
+
 class PermissionComparisonNode(template.Node):
-    """Implements a node to provide an if current user has passed permission 
-    for current object.
+    """Implements a node to provide an if current user has passed permission
+    for passed object.
     """
     @classmethod
     def handle_token(cls, parser, token):
         bits = token.contents.split()
-        if len(bits) != 2:
+        if len(bits) != 3:
             raise template.TemplateSyntaxError(
-                "'%s' tag takes one argument" % bits[0])
+                "'%s' tag takes two arguments" % bits[0])
         end_tag = 'endifhasperm'
         nodelist_true = parser.parse(('else', end_tag))
         token = parser.next_token()
@@ -23,15 +25,16 @@ class PermissionComparisonNode(template.Node):
         else:
             nodelist_false = ""
 
-        return cls(bits[1], nodelist_true, nodelist_false)
+        return cls(bits[1], bits[2], nodelist_true, nodelist_false)
 
-    def __init__(self, codename, nodelist_true, nodelist_false):
+    def __init__(self, obj, codename, nodelist_true, nodelist_false):
+        self.obj = Variable(obj)
         self.codename = codename
         self.nodelist_true = nodelist_true
         self.nodelist_false = nodelist_false
 
     def render(self, context):
-        obj = context.get("obj")
+        obj = self.obj.resolve(context)
         request = context.get("request")
         if permissions.utils.has_permission(obj, request.user, self.codename):
             return self.nodelist_true.render(context)
@@ -43,4 +46,3 @@ def ifhasperm(parser, token):
     """This function provides functionality for the 'ifhasperm' template tag.
     """
     return PermissionComparisonNode.handle_token(parser, token)
-
